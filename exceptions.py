b72c63c694662a7692183abdccc138367d995d52
makarworld <58076271+makarworld@users.noreply.github.com>
2020-10-22 22:20:33 +0300

    Exceptions file

---
+++ exceptions.py
@@ -0,0 +1,14 @@
+# exception from dep bot
+
+
+class AccsNotFound(Exception):
+	pass
+
+class InvalidApi(Exception):
+	pass
+
+class InvalidAmount(Exception):
+	pass
+
+class InvalidSettings(Exception):
+	pass
